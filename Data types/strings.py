15b83ad1453b75ddd9c251d7a83f38871313a831
Davidchuck <rawlencendirangu@gmail.com>
2023-10-01 21:21:40 +0300

    Strings and multiple lines

---
+++ Data types/strings.py
@@ -0,0 +1,22 @@
+#Handling differnt types of strings
+course='Python course for begginers'
+print(course)
+
+course2='Python course for "beginners "'
+course3="Python course for \"beginners\" "
+print (course2)
+print ("\n") #new line
+print(course3, "\n")  
+
+
+#printing multiple line string
+email='''Hello Team
+
+Your issue was resolved.
+Please check to confirm
+
+Regards,
+Tech support Team
+
+'''
+print(email)
