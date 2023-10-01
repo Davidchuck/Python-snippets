bc7f32a20a5637e034ebdc4cb9a17282a375426c
Davidchuck <rawlencendirangu@gmail.com>
2023-10-01 21:08:57 +0300

    first commit

---
+++ Data types/input_function.py
@@ -0,0 +1,11 @@
+#Input functions
+name=input("What is your name? ")
+fav_color=input("What is your favourite color? ")
+print(name, "Likes", fav_color)
+
+#converting string to int and operating on the result
+
+weight=int(input("What is your weight in pounds? "))
+weight_inkg=weight * 0.453592
+print ("Your weight in kilograms is:", weight_inkg, 'kg')
+
