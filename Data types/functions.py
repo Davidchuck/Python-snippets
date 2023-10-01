8ad8d13a5fe62497cc764612fefd89b349127d7f
Davidchuck <rawlencendirangu@gmail.com>
2023-10-01 22:16:28 +0300

    String manipulation functions

---
+++ Data types/functions.py
@@ -0,0 +1,19 @@
+#Functions and method objects in python
+course="Python for Beginners"
+
+#To return the length of course
+print(len(course))
+
+#To convert course to uppercase
+print(course.upper())
+
+#To convert course to lowercase
+print(course.lower())
+
+#To find thw word python in course
+print(course.find('y')) # Returns the index of the passed subject
+#To replace 'python' with 'Java'
+print(course.replace("Python","Java"))
+
+#To check whether Beginners is in course
+print('Beginners' in course) #returns true if yes and False if not
