dc3a8ffd4c45b4630650b00611cfaaf4c2a58e23
Davidchuck <rawlencendirangu@gmail.com>
2024-01-24 06:14:02 +0300

    tuples and unpakcaging

---
+++ Data types/lists/tuples.py
@@ -0,0 +1,9 @@
+#using tuples
+numbers=(2,4,44,23,12,1,23)
+
+#returns position of first appearance of an item in a list
+print(numbers.index(23))
+
+#count number of repeats of an item in a list
+print(numbers.count(2))
+
---
+++ Data types/lists/unpackaging.py
@@ -0,0 +1,4 @@
+#unpackaging
+list=(2,3,4,5)
+x,y,z,w=list #assigns x to 2, y to 3, z to 4 and w to 5
+print(w)
