062412547d5322e9d655b7388222291ee5494e81
Davidchuck <rawlencendirangu@gmail.com>
2024-03-30 13:17:39 +0300

    Products model creation, migration creation and running to update the database

--- Pyshop/pyshop/products/models.py
+++ Pyshop/pyshop/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    #define attributes
+    name=models.CharField(max_length=255)
+    price=models.FloatField()
+    stock=models.IntegerField()
+    image_url=models.CharField(max_length=2083)
+
+
+
+
