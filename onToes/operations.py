5882d7904a83405e26038b16127ecb4859576ee4
Davidchuck <rawlencendirangu@gmail.com>
2024-04-02 22:21:48 +0300

    Python Operations reminder

---
+++ onToes/operations.py
@@ -0,0 +1,10 @@
+number1=int(input("Enter the first Number\n"))
+number2=int(input("Enter the Second Number\n"))
+Sumresult=number1+number2
+Multiplication=number1*number2
+Division=number1/number2
+Subtraction=number1-number2
+Modulus=number1%number2
+
+print("The sum of",number1 ,"and", number2, "is", Sumresult, "\n", 
+      "Multiplication is \n", Multiplication)
